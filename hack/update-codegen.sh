e1a3c2e965c08af1e3b4acb91910a9448092607f
nkwangjun <guaklw@163.com>
2021-10-18 10:10:15 +0800

    fix update-codegen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -13,5 +13,9 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   k8s.io/application-aware-controller/pkg/generated k8s.io/application-aware-controller/pkg/apis \
-  samplecontroller:v1alpha1 \
+  application-aware-controller:v1 \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
+   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
+
+# To use your own boilerplate text append:
+#   --go-header-file "${SCRIPT_ROOT}"/hack/custom-boilerplate.go.txt
