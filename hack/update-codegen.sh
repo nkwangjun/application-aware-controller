335a28808e29ca931e829536fa9f7a17ff617915
nkwangjun <guaklw@163.com>
2021-10-18 11:30:06 +0800

    add auto generated code for appawarecontroller

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -12,8 +12,15 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  k8s.io/application-aware-controller/pkg/generated k8s.io/application-aware-controller/pkg/apis \
+  k8s.io/application-aware-controller/pkg/client k8s.io/application-aware-controller/pkg/apis \
   appawarecontroller:v1 \
+  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
+
+# just test for samplecontroller
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+  k8s.io/sample-controller/pkg/generated k8s.io/sample-controller/pkg/apis \
+  samplecontroller:v1alpha1 \
   --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
